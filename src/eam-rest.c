9731e5ba029f4dc8c5199b5a4e1bb2c124d46320
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2014-03-31 18:08:52 +0200

    rest: REST API URLs builders

---
+++ src/eam-rest.c
@@ -0,0 +1,67 @@
+/* Copyright 2014 Endless Mobile, Inc. */
+
+#include "eam-rest.h"
+#include "eam-os.h"
+#include "eam-config.h"
+
+static const gchar *METHODS_V1_FORMAT[] = {
+  "%s/api/v1/updates/:%s",         /* ALL_UPDATES     */
+  "%s/api/v1/updates/:%s/:%s",     /* APP_UPDATE      */
+  "%s/api/v1/updates/:%s/:%s/:%s", /* APP_UPDATE_LINK */
+  "%s/api/v1/updates/blob/%s",     /* APP_UPDATE_BLOB */
+};
+
+static gchar *
+build_uri_v1 (EamRestMethod method, gchar *saddr, va_list args)
+{
+  const gchar *osver = eam_os_get_version ();
+
+  switch (method) {
+  case EAM_REST_API_V1_GET_ALL_UPDATES:{
+    return g_strdup_printf (METHODS_V1_FORMAT[method], saddr, osver);
+  }
+  case EAM_REST_API_V1_GET_APP_UPDATES:{
+    const gchar *appid = va_arg (args, const gchar *);
+    if (appid)
+      return g_strdup_printf (METHODS_V1_FORMAT[method], saddr, osver, appid);
+  }
+  case EAM_REST_API_V1_GET_APP_UPDATE_LINK:{
+    const gchar *appid = va_arg (args, const gchar *);
+    const gchar *appver = va_arg (args, const gchar *);
+    if (appid && appver)
+      return g_strdup_printf (METHODS_V1_FORMAT[method], saddr, osver, appid, appver);
+  }
+  case EAM_REST_API_V1_GET_APP_UPDATE_BLOB:{
+    const gchar *hash = va_arg (args, const gchar *);
+    if (hash)
+      return g_strdup_printf (METHODS_V1_FORMAT[method], saddr, hash);
+  }
+  default:
+    break;
+  }
+
+  return NULL;
+}
+
+/**
+ * eam_rest_build_uri:
+ * @method: The #EamRestMethod to construct
+ * @...: The parameters of the rest method
+ *
+ * Construct the URI string given the @method
+ *
+ * Returns: a newly-allocated string.
+ **/
+gchar *
+eam_rest_build_uri (EamRestMethod method, ...)
+{
+  va_list args;
+  EamConfig *cfg = eam_config_get ();
+
+  va_start (args, method);
+
+  if (g_strcmp0 (cfg->protver, "v1") == 0)
+    return build_uri_v1 (method, cfg->saddr, args);
+
+  return NULL;
+}
---
+++ tests/rest.c
@@ -0,0 +1,68 @@
+/* Copyright 2014 Endless Mobile, Inc. */
+
+#include <glib.h>
+#include <string.h>
+#include <eam-rest.h>
+#include <eam-config.h>
+
+#define config "[eam]\nappdir = /endless\n" \
+  "downloaddir=/var/tmp\n" \
+  "serveraddress = http://localhost\n"
+
+static void
+load_config (const gchar *protver)
+{
+  EamConfig *cfg = eam_config_get ();
+  GKeyFile *keyfile = g_key_file_new ();
+
+  g_key_file_load_from_data (keyfile, config, strlen (config), G_KEY_FILE_NONE, NULL);
+  eam_config_load (cfg, keyfile);
+  g_key_file_unref (keyfile);
+
+  eam_config_set (cfg, NULL, NULL, NULL, g_strdup (protver));
+}
+
+static void
+test_restv1_basic (void)
+{
+  gchar *uri;
+
+  load_config ("v1");
+
+  uri = eam_rest_build_uri (EAM_REST_API_V1_GET_ALL_UPDATES, NULL);
+  g_assert_cmpstr (uri, ==, "http://localhost/api/v1/updates/:1eos1");
+  g_free (uri);
+
+  uri = eam_rest_build_uri (EAM_REST_API_V1_GET_APP_UPDATES, "com.application.id1", NULL);
+  g_assert_cmpstr (uri, ==, "http://localhost/api/v1/updates/:1eos1/:com.application.id1");
+  g_free (uri);
+
+  uri = eam_rest_build_uri (EAM_REST_API_V1_GET_APP_UPDATE_LINK, "com.application.id2", "2.22", NULL);
+  g_assert_cmpstr (uri, ==, "http://localhost/api/v1/updates/:1eos1/:com.application.id2/:2.22");
+  g_free (uri);
+
+  uri = eam_rest_build_uri (EAM_REST_API_V1_GET_APP_UPDATE_BLOB, "bbccddee-2.22-full", NULL);
+  g_assert_cmpstr (uri, ==, "http://localhost/api/v1/updates/blob/bbccddee-2.22-full");
+  g_free (uri);
+}
+
+static void
+test_restv2_basic (void)
+{
+  load_config ("v2");
+
+  gchar *uri = eam_rest_build_uri (EAM_REST_API_V1_GET_ALL_UPDATES, NULL);
+  g_assert_null (uri);
+}
+
+
+int
+main (int argc, char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/restv1/basic", test_restv1_basic);
+  g_test_add_func ("/restv2/basic", test_restv2_basic);
+
+  return g_test_run ();
+}
