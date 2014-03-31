4ea41c2e9fb27176239076dbc23862e0d61ffaf2
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2014-03-31 13:09:39 +0200

    os: dummy functions for EOS query
    This file queries the EOS to find the OS version and the personality.

---
+++ src/eam-os.c
@@ -0,0 +1,34 @@
+/* Copyright 2014 Endless Mobile, Inc. */
+
+#include "eam-os.h"
+
+
+/**
+ * eam_os_get_version:
+ *
+ * Queries the EndlessOS version.
+ *
+ * @FIXME: This a dummy function!
+ *
+ * Returns: A string with the EOS version
+ **/
+const gchar *
+eam_os_get_version ()
+{
+  return "1eos1";
+}
+
+/**
+ * eam_os_get_personality:
+ *
+ * Queries the EndlessOS personality.
+ *
+ * @FIXME: This a dummy function!
+ *
+ * Returns: A string with the EOS personality
+ **/
+const gchar *
+eam_os_get_personality ()
+{
+  return "Global";
+}
---
+++ src/eam-os.h
@@ -0,0 +1,15 @@
+/* Copyright 2014 Endless Mobile, Inc. */
+
+#ifndef EAM_OS_H
+#define EAM_OS_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+const gchar    *eam_os_get_version                                ();
+const gchar    *eam_os_get_personality                            ();
+
+G_END_DECLS
+
+#endif /* EAM_OS_H */
