aa4e7f8cd021c3d2c5a67f7ecf33cfe9016f57d5
Antia Puentes <apuentes@igalia.com>
2014-04-14 18:37:24 +0200

    Added scripts to rollback a failed application installation

---
+++ scripts/install_rollback/01-delete-symlinks.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Copyright 2014 Endless Mobile, Inc.
+#
+# This script deletes symbolic links that point
+# to the application metadata files like .desktop files,
+# desktop icons, gsettings files and D-Bus service files.
+#
+# Usage: ./01-delete-symlinks.sh <app_id> ...
+#
+# Parameters:
+# <app_id>: ID of the application which installation failed.
+#
+# IMPORTANT: This script makes some assumptions that could
+# be subject of modification:
+# - The application is installed in ${PREFIX}/<app_id>, i.e
+# the APP_PREFIX.
+# - The application metadata lives in the directories:
+#   * ${APP_PREFIX}/${APP_DESKTOP_FILES_SUBDIR} (.desktop files).
+#   * ${APP_PREFIX}/${APP_DESKTOP_ICONS_SUBDIR} (desktop icons)
+#   * ${APP_PREFIX}/${APP_GSETTINGS_SUBDIR} (gschema.xml files)
+#   * ${APP_PREFIX}/${APP_DBUS_SERVICES_SUBDIR} (D-Bus services)
+# - The OS looks for the applications metadata in the
+#   directories:
+#   * ${OS_DESKTOP_FILES_DIR} (.desktop files).
+#   * ${OS_DESKTOP_ICONS_DIR} (desktop icons)
+#   * ${OS_GSETTINGS_DIR} (gschema.xml files)
+#   * ${OS_DBUS_SERVICES_DIR} (D-Bus services)
+#  The symbolic links to the application metadata present on those
+#  directories will be deleted.
+
+# This script uses the configuration variables defined in
+# ../install-config.sh:
+#  * PREFIX
+#  * APP_DESKTOP_FILES_SUBDIR
+#  * APP_DESKTOP_ICONS_SUBDIR
+#  * APP_GSETTINGS_SUBDIR
+#  * APP_DBUS_SERVICES_SUBDIR
+#  * OS_DESKTOP_FILES_DIR
+#  * OS_DESKTOP_ICONS_DIR
+#  * OS_GSETTINGS_DIR
+#  * OS_DBUS_SERVICES_DIR
+
+SCRIPT_DIR=${BASH_SOURCE[0]%/*}
+
+# Include configuration variables
+. ${SCRIPT_DIR}/../install-config.sh
+
+# Include utilities
+. ${SCRIPT_DIR}/../utils.sh
+
+ROOT_UID=0
+if [ "$UID" -ne "$ROOT_UID" ]
+ then
+   exit_error "Must be root to delete the symlinks."
+fi
+
+RM=$(which rm)   || exit_error "Can't find rm"
+
+ARGS=1
+if [ $# -lt "$ARGS" ]
+then
+  exit_error "Usage: `basename $0` <app_id> .."
+fi
+
+APP_ID=$1
+APP_PREFIX="${PREFIX}/${APP_ID}"
+
+delete_symbolic_links() {
+    target_dir=$1; links_dir=$2
+
+    for file in `ls ${links_dir}`
+    do
+        if [ -L "${links_dir}/${file}" ]; then
+            if [ "${links_dir}/${file}" -ef "${target_dir}/${file}" ]; then
+                $RM "${links_dir}/${file}"
+                  if [ "$?" -ne 0 ]; then
+                      exit_error "Failed to delete symbolic link '${links_dir}/${file}' to '${target_dir}/${file}'"
+                  fi
+            fi
+        fi
+    done
+}
+
+
+# Symbolic links for .desktop files
+delete_symbolic_links "${APP_PREFIX}/${APP_DESKTOP_FILES_SUBDIR}" "${OS_DESKTOP_FILES_DIR}"
+
+# Symbolic links for desktop icons
+delete_symbolic_links "${APP_PREFIX}/${APP_DESKTOP_ICONS_SUBDIR}" "${OS_DESKTOP_ICONS_DIR}"
+
+# Symbolic links for GSettings
+delete_symbolic_links "${APP_PREFIX}/${APP_GSETTINGS_SUBDIR}" "${OS_GSETTINGS_DIR}"
+
+# Symbolic links for DBUS services
+delete_symbolic_links "${APP_PREFIX}/${APP_DBUS_SERVICES_SUBDIR}" "${OS_DBUS_SERVICES_DIR}"
+
+exit 0
---
+++ scripts/install_rollback/02-cleanup.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Copyright 2014 Endless Mobile, Inc.
+#
+# This script deletes the bundle and other files downloaded
+# from the server during an application installation proccess.
+#
+# Usage: ./02-cleanup.sh <app_id> <bundle_path> ...
+#
+# Parameters:
+# <app_id>: ID of the application to install.
+# <bundle_path>: Path to the downloaded application bundle.
+#
+# IMPORTANT: This script makes some assumptions that could be subject of
+# modification:
+# - The SHA256 file is in the same directory than the downloaded bundle and
+#   its name is <app_id>.sha256
+
+# This script uses the configuration variables defined in
+# ../install-config.sh:
+#  * PREFIX
+#  * TMP
+
+# Include utilities
+SCRIPT_DIR=${BASH_SOURCE[0]%/*}
+. ${SCRIPT_DIR}/../utils.sh
+
+# ROOT_UID=0
+# if [ "$UID" -ne "$ROOT_UID" ]
+# then
+#   exit_error "Must be root to delete the application files."
+# fi
+
+RM=$(which rm) || exit_error "Can't find rm"
+
+ARGS=2
+if [ $# -lt "$ARGS" ]
+then
+  echo "Usage: `basename $0` <app_id> <bundle_path>"
+  exit $E_BADARGS
+fi
+
+APP_ID=$1
+
+# Delete the installation directory
+if [ -d "${PREFIX}/${APP_ID}" ]; then
+  $RM --recursive --force "${PREFIX}/${APP_ID}"
+  if [ "$?" -ne 0 ]; then
+    exit_error "To delete the installation directory '${PREFIX}/${APP_ID}' failed"
+  fi
+fi
+
+# Delete the temporary directory (if exists)
+if [ -d "${TMP}/${APP_ID}" ]; then
+  $RM --recursive --force "${TMP}/${APP_ID}"
+  if [ "$?" -ne 0 ]; then
+    warning "To delete '${TMP}/${APP_ID}' failed"
+  fi
+fi
+
+# Delete dowloaded files (the bundle and the SHA256 file)
+BUNDLE_FILE=$2
+BUNDLE_DIR=${BUNDLE_FILE%/*}
+SHA256_FILE="${BUNDLE_DIR}/${APP_ID}.sha256"
+
+$RM $SHA256_FILE
+if [ "$?" -ne 0 ]; then
+  warning "Failed to remove the sha256file '${SHA256_FILE}'"
+fi
+
+$RM $BUNDLE_FILE
+if [ "$?" -ne 0 ]; then
+  warning "Failed to remove the bundle file '${BUNDLE_FILE}'"
+fi
+
+exit 0
