fcfe9db803bc8cd75bae965c6194cc2c38e64c86
Srdjan Grubor <sgnn7@sgnn7.org>
2014-04-24 13:57:11 -0500

    Added desktop file creation, converted to python3, and added path fixing

--- tools/deb2bundle.py
+++ tools/deb2bundle.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import argparse
 import tarfile
-from os import path, mkdir, listdir, getcwd, rmdir
+import fileinput
+from os import path, mkdir, listdir, getcwd, rmdir, makedirs, walk
 from shutil import rmtree, move
 from subprocess import Popen, PIPE
 from collections import namedtuple, OrderedDict
@@ -32,7 +33,7 @@ def system_exec(command, directory=None, show_output=True, ignore_error=False):
         error = error.strip()
 
         if show_output and len(output) > 0:
-            print output
+            print(output)
             sys.stdout.flush()
 
         if process.returncode != 0 and not ignore_error:
@@ -42,9 +43,9 @@ def system_exec(command, directory=None, show_output=True, ignore_error=False):
         return ReturnInfo(process.returncode, output, error)
 
     except Exception as err:
-        print >>sys.stderr, Color.RED + "Could not execute", command
-        print >>sys.stderr, err, Color.END
-        print >>sys.stderr, "Terminating early"
+        sys.stderr.write(Color.RED + "Could not execute", command)
+        sys.stderr.write(err, Color.END)
+        sys.stderr.write("Terminating early")
         exit(1)
 
 # Allows for invoking attributes as methods/functions
@@ -72,30 +73,68 @@ class BundleConverter(object):
     def _extract_bundle_data(self, deb_package):
         field_values = AttributeDict()
 
-        print get_color_str("-" * 40, Color.GREEN)
+        print(get_color_str("-" * 40, Color.GREEN))
         for info_key in BUNDLE_METADATA.keys():
             field_name = BUNDLE_METADATA[info_key]
 
-            field_values[info_key] = system_exec("dpkg-deb -f %s %s" % (deb_package, field_name), show_output=False).output
+            field_values[info_key] = system_exec("dpkg-deb -f %s %s" % (deb_package, field_name), show_output=False).output.decode(encoding='UTF-8')
 
-            print info_key, "\t", field_values[info_key]
-        print get_color_str("-" * 40, Color.GREEN)
+            print(info_key, "\t", field_values[info_key])
+        print(get_color_str("-" * 40, Color.GREEN))
 
         return field_values
 
+    def _fix_path(self, filename, bundle_info):
+        with fileinput.FileInput(filename,inplace=True) as launcher_file:
+            for line in launcher_file:
+                line = line.replace("/usr", "/endless")
+                line = line.replace(bundle_info.orig_pkg_name, bundle_info.appid)
+                print(line, end='')
+
+
+    def _fix_launcher_paths(self, launcher, bin_path, bundle_info):
+        self._fix_path(path.join(bin_path, launcher), bundle_info)
+
+    def _fix_source_paths(self, src_file, bundle_info):
+        self._fix_path(src_file, bundle_info)
+
+    def _add_desktop_file(self, launcher, applications_dir, bundle_info):
+        launcher_name = launcher + ".desktop"
+        desktop_launcher = path.join(applications_dir, launcher_name)
+
+        pkg_name = bundle_info.orig_pkg_name.replace('eos-','')
+
+        exec_line = path.join("/endless", bundle_info.appid, 'bin', path.basename(launcher))
+        icon = "eos-app-%s" % pkg_name
+
+        makedirs(applications_dir)
+
+        print("applications_dir %s" % applications_dir)
+        with open(desktop_launcher, 'w') as desktop_file:
+            desktop_file.write("[Desktop Entry]\n")
+            desktop_file.write("Version=%s\n" % bundle_info.version)
+            desktop_file.write("Name=%s\n" % pkg_name.title())
+            desktop_file.write("Comment=%s\n" % pkg_name.title())
+            desktop_file.write("Type=Application\n")
+            desktop_file.write("Exec=%s\n" % exec_line)
+            desktop_file.write("Icon=%s\n" % icon)
+            desktop_file.write("Categories=Utility;\n")
+            desktop_file.write("X-Endless-ShowInAppStore=true\n")
+            desktop_file.write("X-Endless-SplashScreen=false")
+
     def convert(self):
         if not path.isfile(self.args.deb_package):
-            print >>sys.stderr, 'File not found:',  self.args.deb_package
+            sys.stderr.write("File not found:",  self.args.deb_package)
             exit(1)
 
         # Get bundle metadata
         bundle_info = self._extract_bundle_data(self.args.deb_package)
 
-        original_pkg_name = bundle_info.appid
+        bundle_info.orig_pkg_name = bundle_info.appid
         # Fix eos-* named packages
         if bundle_info.appid.startswith('eos-'):
             bundle_info.appid = "com.endlessm.%s" % bundle_info.appid.replace('eos-','', 1)
-            print get_color_str("WARN: New package name is %s" % bundle_info.appid, Color.YELLOW)
+            print(get_color_str("WARN: New package name is %s" % bundle_info.appid, Color.YELLOW))
 
         # Cretate a temp dir
         working_dir = mkdtemp(prefix='deb2bundle_')
@@ -103,17 +142,38 @@ class BundleConverter(object):
         # Create package-named dir
         extraction_dir = path.join(working_dir, bundle_info.appid)
         mkdir(extraction_dir)
-        print "Using", get_color_str(extraction_dir, Color.GREEN), "as staging area"
+        print("Using", get_color_str(extraction_dir, Color.GREEN), "as staging area")
 
-        print "Extracting..."
+        print("Extracting...")
         # TODO: Convert this to native python
         system_exec("dpkg --extract %s %s" % (self.args.deb_package, extraction_dir))
 
-        print "Moving /usr/* one level up"
+        print("Moving /usr/* one level up")
         user_dir = path.join(extraction_dir, 'usr')
         for item in listdir(user_dir):
             move(path.join(user_dir, item), extraction_dir)
         rmdir(user_dir)
+
+        # Fix things in launcher instead of fixing --prefix
+        applications_dir = path.join(extraction_dir, 'share/applications')
+        if not path.exists(applications_dir):
+            print("No .desktop launcher. Applying some black magic...")
+
+            print("Fixing paths")
+            source_files = []
+            for root, sub_folders, files in walk(extraction_dir):
+                for source_file in files:
+                    if source_file.endswith('.js'):
+                        source_files.append(path.join(root,source_file))
+
+            for filename in source_files:
+                self._fix_source_paths(filename, bundle_info)
+
+            print("Adding a launcher")
+            bin_path = path.join(extraction_dir, 'bin')
+            for launcher in listdir(bin_path):
+                print("  Fixing launcher: %s" % launcher)
+                self._add_desktop_file(launcher, applications_dir, bundle_info)
 
         # Use full package name if regular run and simple .tgz if debugging
         if not self.args.debug:
@@ -121,9 +181,9 @@ class BundleConverter(object):
         else:
             target_path = "%s.tgz" % (bundle_info.appid)
 
-        print "Compressing data to %s" % get_color_str(target_path, Color.GREEN)
+        print("Compressing data to %s" % get_color_str(target_path, Color.GREEN))
 
-        print "  - Writing metadata..."
+        print("  - Writing metadata...")
         metadata_path = path.join(extraction_dir, '.info')
         with open(metadata_path, 'w') as metadata:
             metadata.write(BUNDLE_METADATA_HEADER)
@@ -131,14 +191,14 @@ class BundleConverter(object):
             for item in bundle_info.keys():
                 metadata.write("%s=%s\n" % (item, bundle_info[item]))
 
-        print "  - Writing files..."
+        print("  - Writing files...")
         with tarfile.open(target_path, 'w:gz', format=tarfile.GNU_FORMAT) as bundle:
             bundle.add(extraction_dir, arcname=bundle_info.appid)
 
-        print "Cleaning up..."
+        print("Cleaning up...")
         rmtree(working_dir)
 
-        print "Done"
+        print("Done")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Converts Debian packages into application bundles')
