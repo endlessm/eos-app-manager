20945d5cfe1c80c4f567305d1c4eea2cadc0727e
Antia Puentes <apuentes@igalia.com>
2014-04-30 12:58:25 +0200

    scripts: fix missing include

--- scripts/install_rollback/02-cleanup.sh
+++ scripts/install_rollback/02-cleanup.sh
@@ -21,8 +21,12 @@
 #  * PREFIX
 #  * TMP
 
-# Include utilities
 SCRIPT_DIR=${BASH_SOURCE[0]%/*}
+
+# Include configuration variables
+. ${SCRIPT_DIR}/../install-config.sh
+
+# Include utilities
 . ${SCRIPT_DIR}/../utils.sh
 
 debug "Running '${BASH_SOURCE[0]}'"
