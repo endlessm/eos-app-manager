ae4ad07340353f557359d2c74db843268c1494d7
Srdjan Grubor <sgnn7@sgnn7.org>
2014-05-14 13:16:22 -0500

    Fixed a typo in the desktop file generation

--- tools/deb2bundle.py
+++ tools/deb2bundle.py
@@ -131,7 +131,7 @@ class BundleConverter(object):
             desktop_file.write("Icon=%s\n" % icon)
             desktop_file.write("Categories=Utility;\n")
             desktop_file.write("X-Endless-ShowInAppStore=true\n")
-            desktop_file.write("X-Endless-SplashScreen=false")
+            desktop_file.write("X-Endless-SplashScreen=false\n")
             desktop_file.write("DBusActivatable=true")
 
     def convert(self):
