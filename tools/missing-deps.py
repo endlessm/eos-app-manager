1d04852b23967d26f6fafca9251b76e3789b6d44
Roddy Shuler <roddy@endlessm.com>
2014-06-10 16:43:03 -0700

    Fixed missing deps script for virtual packages
    Include a hard-coded work-around to ensure we ignore a few packages
    that are available in apt but not installable due to dependency issues.
    In these cases, we need to ensure that we choose the alternate option
    to satisfy the dependency list.
    
    We also now allow specification of either the debian package name
    or a .deb file name.
    
    The default core is now none rather than eos-core, so that the default
    is simply to list all the dependencies of a package.
    
    Output is now sorted alphabetically.
    
    [endlessm/eos-shell#2633]

--- tools/missing-deps.py
+++ tools/missing-deps.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
-# Note: for the intended use case, the debian package will not be
-# published to the apt repo yet, so we need to process the debian file
-# rather than checking the apt repo using the debian package name
-
 import apt
 import apt.debfile
 import argparse
 import os
 import sys
 
-VERSION="0.1"
+VERSION = "0.1"
+
+# TODO Really should have a way to detect that we are unable to satisfy
+# dependencies for these packages and automatically choose a different
+# option from the list of candidate dependencies; for now, we just ignore
+# the ones that we know cause a problem for eos-core
+IGNORE_PKGS = ['gnome-shell',
+               'xserver-xorg-video-all',
+               'xserver-xorg-input-all']
 
 # Allows for invoking attributes as methods/functions
 class AttributeDict(dict):
@@ -25,59 +29,79 @@ class DependencyChecker(object):
         self._core_name = core_name
         self._cache = apt.Cache()
         self._core_deps = []
-        self._find_deps(self._core_deps, self._core_name)
+        if self._core_name:
+            self._core_deps.append(self._core_name)
+            self._find_deps(self._core_deps, self._core_name)
+            self._core_deps.sort()
 
+    # Find the real package that provides the list of options,
+    # which may include virtual packages
+    def _find_providing_pkg(self, pkg_list):
+        for pkg in pkg_list:
+            try:
+                # For Cache providing packages and candidate dependencies,
+                # each entry is a dictionary with a name
+                pkg_name = pkg.name
+            except AttributeError:
+                # For DebPackage depends, each entry is a tuple
+                # with name as the first element
+                pkg_name = pkg[0]
+            if pkg_name in IGNORE_PKGS:
+                # Special cases that we know are not installable
+                # for which we should choose a different option
+                continue
+            if self._cache.is_virtual_package(pkg_name):
+                providers = self._cache.get_providing_packages(pkg_name)
+                providing_pkg = self._find_providing_pkg(providers)
+                if providing_pkg:
+                    return providing_pkg
+            elif self._cache.has_key(pkg_name):
+                return pkg_name
+        return None
+
+    # Recursively find all the dependencies of the package
+    # (either .deb file or package name -- not a virtual package)
+    # and append to the provided deps list
     def _find_deps(self, deps, pkg_name):
-        if pkg_name not in deps:
-            deps.append(pkg_name)
+        if pkg_name.endswith('.deb'):
+            # Debian package file
+            if not os.path.isfile(pkg_name):
+                raise Exception(pkg_name + ': file not found')
+            deb = apt.debfile.DebPackage(pkg_name)
+            deb.check()
+            direct_deps = deb.depends
+        else:
+            # Not a .deb file: assume it is a package name
+            if not self._cache.has_key(pkg_name):
+                raise Exception(pkg_name + ': package not found in cache')
             pkg = self._cache[pkg_name]
-            for dep_list in pkg.candidate.dependencies:
-                # The dep_list is a list of possibilities,
-                # only one of which is required
-                for dep in dep_list:
-                    dep_name = dep.name
-                    if self._cache.has_key(dep_name):
-                        self._find_deps(deps, dep_name)
-                        break;
-                else:
-                    # No candidate found in cache
-                    # TODO: should raise an exception,
-                    # but need to troubleshoot some edge cases first
-                    print('No candidate found in cache for any of ' +
-                          str(dep_list) + ' required by ' + pkg_name)
+            direct_deps = pkg.candidate.dependencies
+        for dep_list in direct_deps:
+            # The dep_list is a list of possible packages,
+            # real and/or virtual, only one of which is required
+            real_pkg = self._find_providing_pkg(dep_list)
+            if not real_pkg:
+                raise Exception(pkg_name + ': no candidate found for any of ' +
+                                str(dep_list))
+            # If not already processed, add the real package to the overall
+            # dependencies and find all of its recursive dependencies
+            if not real_pkg in deps:
+                deps.append(real_pkg)
+                self._find_deps(deps, real_pkg)
 
-    def find_missing_deps(self, deb_package):
-        if not os.path.isfile(deb_package):
-            sys.stderr.write("File not found: " + deb_package + "\n")
-            exit(1)
-
+    # Find all the deps of the specified package (name or file) that are not
+    # already present in the system as a dependency of the core package
+    def find_missing_deps(self, pkg_name):
         # Recursively find all the dependencies of the package
-        deb = apt.debfile.DebPackage(deb_package)
-        deb.check()
-        top_deps = deb.depends
-        deb_deps = []
-        for dep_list in top_deps:
-            # The dep_list is a list of possibilities,
-            # only one of which is required
-            for dep in dep_list:
-                dep_name = dep[0]
-                if self._cache.has_key(dep_name):
-                    self._find_deps(deb_deps, dep_name)
-                    break;
-            else:
-                # No candidate found in cache
-                # TODO: should raise an exception,
-                # but need to troubleshoot some edge cases first
-                print('No candidate found in cache for any of ' +
-                      str(dep_list) + ' required by ' + deb_package)
-
+        pkg_deps = []
+        self._find_deps(pkg_deps, pkg_name)
+        pkg_deps.sort()
         # Filter out the debian dependencies that are in the
         # core dependency tree
         missing_deps = []
-        for deb_dep in deb_deps:
-            if deb_dep not in self._core_deps:
-                missing_deps.append(deb_dep)
-
+        for pkg_dep in pkg_deps:
+            if pkg_dep not in self._core_deps:
+                missing_deps.append(pkg_dep)
         return missing_deps
 
 if __name__ == '__main__':
@@ -86,12 +110,12 @@ if __name__ == '__main__':
 
     parser.add_argument(
         'deb_package',
-        help = 'Debian package file to analyze')
+        help = 'Debian package name or .deb file to analyze')
 
     parser.add_argument(
         '-c', '--core_name',
-        help = 'Core package name for the installation (default = eos-core)',
-        default = 'eos-core')
+        help = 'Core package name for the installation (default = none)',
+        default = None)
 
     parser.add_argument(
         '--version',
