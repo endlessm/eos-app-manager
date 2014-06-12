058be362ef3a0d3f1b825ad808758bcb352c18ab
Antia Puentes <apuentes@igalia.com>
2014-06-12 12:53:14 +0200

    scripts: cosmetic changes

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -2,22 +2,30 @@
 
 # Copyright 2014 Endless Mobile, Inc.
 #
-# This script contains log utilities.
-# It is meant to be used by other scripts that want to print
-# error, debug messages, and others.
+# This script contains function utilities.
 
-exit_error() {
+# Log
+# ---
+# Print errors, debug messages, and others.
+
+exit_error ()
+{
     message=$1
+
     printf "Error: ${message}\n"
     exit 255
 }
 
-warning(){
+warning ()
+{
     message=$1
+
     printf "Warning: ${message}\n"
 }
 
-debug(){
+debug ()
+{
     message=$1
+
     printf "Debug: ${message}\n"
 }
