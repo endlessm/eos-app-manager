4ff617cfd2ba4acd340beddf4408c60c2fd646f4
Antia Puentes <apuentes@igalia.com>
2014-06-13 10:10:44 +0200

    scripts: fail if the dir does not exist

--- scripts/uninstall/03-delete-installation-dir.sh
+++ scripts/uninstall/03-delete-installation-dir.sh
@@ -16,14 +16,7 @@
 . ${BASH_SOURCE[0]%/*}/../utils.sh
 
 print_header "${BASH_SOURCE[0]}"
-
-RM=$(which rm) || exit_error "Can't find rm"
-
 check_args_minimum_number "${#}" 1 "<app_id>"
 APP_ID=$1
 
-# Delete the installation directory
-$RM --recursive --force "${EAM_PREFIX}/${APP_ID}"
-if [ "$?" -ne 0 ]; then
-  exit_error "To delete the installation directory '${EAM_PREFIX}/${APP_ID}' failed"
-fi
+rm --recursive "${EAM_PREFIX}/${APP_ID}"
