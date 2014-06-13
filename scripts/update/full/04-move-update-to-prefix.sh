a82c334840bed28ce299071265c3b0a7e7b0819f
Antia Puentes <apuentes@igalia.com>
2014-06-13 10:24:55 +0200

    scripts: simplify script

--- scripts/update/full/04-move-update-to-prefix.sh
+++ scripts/update/full/04-move-update-to-prefix.sh
@@ -20,24 +20,7 @@
 . ${BASH_SOURCE[0]%/*}/../../utils.sh
 
 print_header "${BASH_SOURCE[0]}"
-
-MV=$(which mv) || exit_error "Can't find mv"
-
 check_args_minimum_number "${#}" 1 "<app_id>"
 APP_ID=$1
 
-# Move the uncompressed bundle to the installation directory
-if [ ! -d "${EAM_TMP}/${APP_ID}" ]; then
-  exit_error "The extracted bundle is not a directory called '${APP_ID}'"
-fi
-
-if [ -d "${EAM_PREFIX}/${APP_ID}" ]; then
-  # This should never happen.
-  # If it does happen, there is an error in the installation/update process.
-  exit_error "A directory called '${EAM_PREFIX}/${APP_ID}' already exists"
-fi
-
-${MV} --force "${EAM_TMP}/${APP_ID}" "${EAM_PREFIX}"
-if [ "$?" -ne 0 ]; then
-  exit_error "To move the application from '${EAM_TMP}' to '${EAM_PREFIX}' failed"
-fi
+mv "${EAM_TMP}/${APP_ID}" "${EAM_PREFIX}"
