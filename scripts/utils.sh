8af5edc517dc80eb6916c00f3322c51047b2509a
Dan Nicholson <nicholson@endlessm.com>
2014-07-14 09:24:51 -0700

    scripts: Byte compile python modules on install or update
    The python bytecode is not distributed with the bundles, so we need to
    generate it on install or update. We can't let python recursively
    compile the entire app directory since compiling a module with the wrong
    python version will cause an error. Therefore, we need to narrow
    compiling to the python site directories where we know the python
    version.
    
    Since updating or removing the app will delete the entire old app
    directory, we don't need to worry about cleaning up any existing python
    bytecode. The new app installation will operate on a clean directory.
    
    [endlessm/eos-shell#3053]

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -344,3 +344,29 @@ parse_ini_section ()
      < ${config_file} \
       | sed -n -e "/^\[${section}\]/,/^\s*\[/{/^[^;].*\=.*/p;}"`
 }
+
+# python modules
+#---------------
+# Recursively byte compile all python modules in the site directories.
+compile_python_modules ()
+{
+    if [ "$#" -ne 1 ]; then
+        exit_error "compile_python_modules: incorrect number of arguments"
+    fi
+
+    dir=$1
+
+    for python in python2 python3; do
+        for sitedir in dist-packages site-packages; do
+            if [ "${python}" = python2 ]; then
+                pyvers=$(${python} -c 'import sys; print sys.version[0:3]')
+                pydir="${dir}/lib/python${pyvers}/${sitedir}"
+            else
+                pydir="${dir}/lib/${python}/${sitedir}"
+            fi
+            if [ -d "${pydir}" ]; then
+                ${python} -m compileall -f -q "${pydir}"
+            fi
+        done
+    done
+}
