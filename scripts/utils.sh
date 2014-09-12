43d5d8dc3316c9bfa85387825f5d637da989d444
Dan Nicholson <nicholson@endlessm.com>
2014-09-12 16:02:25 -0700

    scripts: Create the shell search provider directory
    It will be created when needed by the symlinking code, but best to
    ensure it's there all the time.

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -403,7 +403,8 @@ create_os_directories ()
         "${OS_BIN_DIR}" "${OS_DESKTOP_FILES_DIR}" \
         "${OS_DESKTOP_ICONS_DIR}" "${OS_GSETTINGS_DIR}" \
         "${OS_DBUS_SERVICES_DIR}" "${OS_HELP_DIR}" \
-        "${OS_EKN_DATA_DIR}" "${OS_EKN_MANIFEST_DIR}"
+        "${OS_EKN_DATA_DIR}" "${OS_EKN_MANIFEST_DIR}" \
+        "${OS_SHELL_SEARCH_DIR}"
     do
         [ -d "${dir}" ] || mkdir --parents "${dir}"
     done
