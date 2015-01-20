4fcc5832c7bc09eb70be11cc4294ffde9630b35e
Srdjan Grubor <sgnn7@sgnn7.org>
2015-01-20 14:52:32 -0600

    Fixed spacing inconsistencies in eam-install.h
    Tabs -> 8 spaces

--- src/eam-install.h
+++ src/eam-install.h
@@ -17,7 +17,7 @@ G_BEGIN_DECLS
 #define EAM_INSTALL_CLASS(k) \
   (G_TYPE_CHECK_CLASS_CAST((k), EAM_TYPE_INSTALL, EamInstallClass))
 
-#define EAM_IS_INSTALL(o)	\
+#define EAM_IS_INSTALL(o)        \
   (G_TYPE_CHECK_INSTANCE_TYPE ((o), EAM_TYPE_INSTALL))
 
 #define EAM_IS_INSTALL_CLASS(k) \
@@ -26,8 +26,8 @@ G_BEGIN_DECLS
 #define EAM_INSTALL_GET_CLASS(o) \
   (G_TYPE_INSTANCE_GET_CLASS ((o), EAM_TYPE_INSTALL, EamInstallClass))
 
-typedef struct _EamInstallClass	EamInstallClass;
-typedef struct _EamInstall	EamInstall;
+typedef struct _EamInstallClass        EamInstallClass;
+typedef struct _EamInstall        EamInstall;
 
 struct _EamInstall
 {
@@ -42,9 +42,9 @@ struct _EamInstallClass
 GType           eam_install_get_type            (void) G_GNUC_CONST;
 
 EamTransaction * eam_install_new                (EamPkgdb    *pkgdb,
-						 const gchar *appid,
-						 EamUpdates  *updates,
-						 GError     **error);
+                                                 const gchar *appid,
+                                                 EamUpdates  *updates,
+                                                 GError     **error);
 
 const char *    eam_install_get_download_url    (EamInstall  *install);
 const char *    eam_install_get_signature_url   (EamInstall  *install);
