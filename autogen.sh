5254dbc3b4b841bf6ed7f3b45c8d695d026eeb94
Emmanuele Bassi <ebassi@gnome.org>
2015-05-20 14:13:43 +0100

    build: Clean up autogen.sh
    Respect NOCONFIGURE's environment variable for autobuilders, and ensure
    srcdir/builddir differences are handled correctly.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-intltoolize --force --copy
-autoreconf --install
-./configure $*
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd "$srcdir"
+
+intltoolize --force --copy || exit $?
+autoreconf --install --force --verbose || exit $?
+
+cd "$olddir"
+
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
