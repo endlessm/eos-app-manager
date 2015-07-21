230bd715cad1733f7e68570b5b047189fda7cce7
Emmanuele Bassi <ebassi@gnome.org>
2015-07-21 17:13:13 +0100

    Revert "config: Plug a leak in the error path"
    This reverts commit 5e599e6846d30f2bc0abf343da725394e7a4f457.
    
    GError is declared as `g_autoptr()`.

--- src/eam-config.c
+++ src/eam-config.c
@@ -145,12 +145,10 @@ eam_config_set_key (const EamConfigKey *key,
       g_assert_not_reached ();
   }
 
-  if (error != NULL) {
+  if (error != NULL)
     eam_log_error_message ("Unable to read configuration key '%s': %s",
                            key->key_name,
                            error->message);
-    g_error_free (error);
-  }
 }
 
 static EamConfig *
