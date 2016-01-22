1f17b725ecbe6034595996c7414f71299b30d88f
Emmanuele Bassi <ebassi@gnome.org>
2016-01-22 15:14:07 +0000

    Add GPLv2 license

--- src/eam-install.h
+++ src/eam-install.h
@@ -1,4 +1,21 @@
-/* Copyright 2014 Endless Mobile, Inc. */
+/* eam-install.h: Installation transaction
+ *
+ * This file is part of eos-app-manager.
+ * Copyright 2014  Endless Mobile Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef EAM_INSTALL_H
 #define EAM_INSTALL_H
--- tools/eam-commands.c
+++ tools/eam-commands.c
@@ -1,3 +1,22 @@
+/* eam: Command line tool for eos-app-manager
+ *
+ * This file is part of eos-app-manager.
+ * Copyright 2014  Endless Mobile Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "eam-commands.h"
 
 const EamToolCommand commands[EAM_N_COMMANDS] = {
