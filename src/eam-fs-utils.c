8d624cffa85b7ee0f205ffd1b0818396237376fb
Jasper St. Pierre <jstpierre@mecheye.net>
2016-04-15 12:10:54 -0700

    eam-fs-utils: Also symlink splash content in bundles
    We are going to start moving splash content to app bundles.

--- src/eam-fs-utils.c
+++ src/eam-fs-utils.c
@@ -60,6 +60,7 @@ static const char *fs_layout[] = {
   [EAM_BUNDLE_DIRECTORY_SHELL_SEARCH] = "share/gnome-shell/search-providers",
   [EAM_BUNDLE_DIRECTORY_KDE4] = "share/kde4",
   [EAM_BUNDLE_DIRECTORY_XDG_AUTOSTART] = "xdg/autostart",
+  [EAM_BUNDLE_DIRECTORY_SPLASH] = "share/eos-shell-content/splash",
 };
 
 const char *
